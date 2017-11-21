d9668301099e80fcfd61db8dfa5d4a92bd11628e
Emilio Schadt <eas764@nyu.edu>
2017-11-20 19:14:07 -0500

    working app

--- app.js
+++ app.js
@@ -1,49 +1,118 @@
-/**
- * Website gives users a list of fake tinder profiles.
- * Half of the users are given regular profiles, the other
- * half are given profiles that are verified STD free. The only
- * data stored on users is their gender and sexual preferences,
- * and whether they swiped left or right on a particular profile
- */
+//database config
+//
+require('./db');
 
-// Bring in all the frameworks
+// basic boiler plate for server
 const express = require('express');
 const app = express();
+
+const mongoose = require("mongoose");
+
+const path = require('path');
 const bodyParser = require('body-parser');
-const hbs = require("hbs");
-const path = require("path");
-const mongoose = require('mongoose');
 
+const session = require('express-session');
 
-// express static setup
-app.use(express.static(path.join(__dirname, 'public')));
-
-// body parser setup
-app.use(bodyParser.urlencoded({ extended: false }));
-
-
-// hbs setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// Our schema
-require('./db');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Our only page
-app.get('/', function(req, res) {
-    // Generate a random number to decide if user gets verified profiles
-    const x = Math.round(Math.random())
+//top stop deprication errors
+//
+mongoose.Promise = global.Promise;
 
-    // Unverified
-    if (x == 0){
+//setup a session
 
-    }
+const sessionOptions = {
+        secret: 'Blade Runner is better than Blade Runner 2049',
+        saveUninitialized: false,
+        resave: false
+};
 
-    // Verified
-    else {
+app.use(session(sessionOptions));
 
-    }
+const Result = mongoose.model("Result");
+
+app.get("/", function(req, res){
+    res.render("survey1");
 });
 
-// Listen in on 3000
+
+app.post("/survey1", function(req, res){
+
+    if (!req.session.result) {
+        req.session.result = {};
+    }
+
+    //validate form
+    //
+    if (req.body.gender === undefined || req.body.interest === undefined) {
+        res.redirect("/");
+    }
+
+    //make new collection
+    //
+    const newResult = new Result({gender: req.body.gender, interest: req.body.interest});
+
+    //random number to decide if survey taker   //1 if with badges //0 without
+    //
+    const verifiedSurvey = Math.floor(Math.random() * 2);
+
+    //render the appropriate survey.
+    //
+    if (newResult.interest === "male") { //show males
+        if (verifiedSurvey === 1) {
+            res.render("survey2", {imagePath: "Verified_Men", both: false, picId: 1, gender: req.body.gender, interest: req.body.interest});
+        }
+        else {
+            res.render("survey2", {imagePath: "Men", both: false, picId: 2, gender: req.body.gender, interest: req.body.interest});
+        }
+    }
+    else if (newResult.interest === "female") { //show females
+        if (verifiedSurvey === 1){
+            res.render("survey2", {imagePath: "Verified_Women", both: false, picId: 3, gender: req.body.gender, interest: req.body.interest});
+        }
+        else {
+            res.render("survey2", {imagePath: "Women", both: false, picId: 4, gender: req.body.gender, interest: req.body.interest});
+        }
+
+    }
+    else { //both
+        if (verifiedSurvey === 1) {
+            res.render("survey2", {imagePath1: "Verified_Women", imagePath2: "Verified_Men", both: true, picId: 5, gender: req.body.gender, interest: req.body.interest});
+        }
+        else {
+            req.session.result.picId = 6;
+            res.render("survey2", {imagePath1: "Women", imagePath2: "Men", both: true, picId: 6, gender: req.body.gender, interest: req.body.interest});
+        }
+
+    }
+
+
+});
+
+app.post("/survey2", function(req, res){
+
+    const collectionToSave = new Result({swipeResults: req.body.person,
+                                            picId: req.body.picId,
+                                            gender: req.body.gender,
+                                            interest: req.body.interest});
+
+    console.log(collectionToSave);
+
+    collectionToSave.save(function(err){
+        if(!err){
+            res.render("thankyou");
+        }
+        else {
+            res.redirect("survey2");
+        }
+    });
+
+});
+
+
+
 app.listen(3000);
--- db.js
+++ db.js
@@ -1,4 +1,27 @@
-/*
-* Database file for profile swipe data
-* */
-var mongoose = require('mongoose');
+// db.js 1st draft DATA MODEL.
+const mongoose = require('mongoose');
+const fs = require('fs');
+
+//const configString = fs.readFileSync("config.json");
+//const configObj = JSON.parse(configString);
+
+//const uri = configObj ? configObj.uri : "mongodb://localhost/SNdata";
+
+const Result = new mongoose.Schema({
+    swipeResults: [Number], //number between 1-15 for which they ppl they swiped right on.
+    //if user chooses to see both genders when swiping, then women are stored 1-15 for right swipes, and men 16-30.
+    picId: Number, //1-6 depending on which set of photos user sees!
+    gender: String,
+    interest: String
+});
+
+
+//register the models
+//
+mongoose.model('Result', Result);
+
+//connect to Database.
+//
+//mongoose.connect(uri, {useMongoClient: true});
+
+mongoose.connect("mongodb://localhost/SNdata", {useMongoClient: true});
